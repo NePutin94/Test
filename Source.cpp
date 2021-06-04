df606606ed26c612f258b09e2386138a0b726d96
NePutin94 <dimkap314@gmail.com>
2021-06-04 09:31:22 +0000

    edrror

--- Source.cpp
+++ Source.cpp
@@ -5,20 +5,22 @@ int main()
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
+        fkhdsjfa
+        in 
+        dsf
     while (window.isOpen())
-    {
+    {adf
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+asdf
         window.clear();
         window.draw(shape);
         window.display();
-    }
+    
 
     return 0;
 }
